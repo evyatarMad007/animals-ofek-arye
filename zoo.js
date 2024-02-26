a4c0fbd8b01e15b63d586aaf61aa0b0655596e05
= <=>
2024-02-26 23:11:13 +0200

    .

--- zoo.js
+++ zoo.js
@@ -1,64 +1,78 @@
-let animalsArry = JSON.parse(localStorage.getItem("animals")); // תפיסת מערך חיות מלוקל סטורג
+// inputs
 const searchAnimalName = document.getElementById("name-search");
 const searchAnimalWeight = document.getElementById("weight-search");
 const searchAnimalHeight = document.getElementById("height-search");
 const colorSearch = document.getElementById("color-search");
+
 const habitatSearchLand = document.getElementById("land-search");
 const habitatSearchSea = document.getElementById("sea-search");
 const pradatorSearchTrue = document.getElementById("is-pradator");
 const pradatorSearchFalse = document.getElementById("not-pradator");
+
+// reset button
 const clearBt = document.getElementById("clear-bt");
 
-if (JSON.parse(localStorage.getItem("filterd array"))) {
-} else {
-  localStorage.setItem("filterd array", JSON.stringify(animalsArry));
-}
+const setupApp = () => {
+  console.log("Setting up the app");
+  const animalsOriginalArray = JSON.parse(localStorage.getItem("animals")) || [];
+  let animalsFilteredArray = JSON.parse(localStorage.getItem("filterd array")) || [...animalsOriginalArray];
+  localStorage.setItem("filterd array", JSON.stringify(animalsFilteredArray));
 
-// colorSearch.addEventListener("change", () => console.log(colorSearch.value));
+  // create util function generic to filter array
+  const filterAnimals = (e, inpType, checkboxValue) => {
+    // let newArray = animalsFilteredArray.filter((animal) => {
+    //   if (inpType === "weight" || inpType === "height")
+    //     return animal[inpType] > e.target.value;
+    //   if (inpType === "habitat") return animal[inpType] === checkboxValue;
+    //   if (inpType === "isPredator") return animal[inpType] === checkboxValue;
+    //   return animal[inpType]
+    //     .toLowerCase()
+    //     .includes(e.target.value.toLowerCase());
+    // });
+    return applyFilter(newArray);
+  };
 
-searchAnimalName.addEventListener("input", arryFilteringName); //האזנה לאירוע שינוי באינפוט
-function arryFilteringName(e) {
-  //פונקצייה שמופעלת על ידי האיוונט ליסטינר
-  // cardDiv.innerHTML = ``; //איפוס הדיב של המבקרים
-  let newArry = animalsArry.filter(function (animal) {
-    // יצירת מערך חדש שיכיל את המערך המסונן
-    return animal.name.toLowerCase().includes(e.target.value.toLowerCase()); //בדיקת הכלה של האותיות שהוזנו והמרה לאותיות קטנות
+  const applyFilter = (newFilteredData) => {
+    console.log("newFilteredData", newFilteredData);
+
+    // animalsFilteredArray = [...newArray];
+    // localStorage.setItem("filterd array", JSON.stringify(newArray));
+  };
+  // add event listeners
+  searchAnimalName.addEventListener("input", (e) => filterAnimals(e, "name"));
+  searchAnimalWeight.addEventListener("input", (e) =>
+    filterAnimals(e, "weight")
+  );
+  searchAnimalHeight.addEventListener("input", (e) =>
+    filterAnimals(e, "height")
+  );
+  colorSearch.addEventListener("input", (e) => filterAnimals(e, "color"));
+  habitatSearchLand.addEventListener("input", (e) =>
+    filterAnimals(e, "habitat", "land")
+  );
+  habitatSearchSea.addEventListener("input", (e) =>
+    filterAnimals(e, "habitat", "sea")
+  );
+  pradatorSearchTrue.addEventListener("input", (e) =>
+    filterAnimals(e, "isPredator", true)
+  );
+  pradatorSearchFalse.addEventListener("input", (e) =>
+    filterAnimals(e, "isPredator", false)
+  );
+
+  // clear button
+  clearBt.addEventListener("click", (e) => {
+    animalsFilteredArray = [...animalsOriginalArray];
+    localStorage.setItem("filterd array", JSON.stringify(animalsFilteredArray));
+    searchAnimalName.value = "";
+    searchAnimalWeight.value = "";
+    searchAnimalHeight.value = "";
+    colorSearch.value = "";
+    habitatSearchLand.checked = false;
+    habitatSearchSea.checked = false;
+    pradatorSearchTrue.checked = false;
+    pradatorSearchFalse.checked = false;
   });
-  localStorage.setItem("filterd array", JSON.stringify(newArry));
-  // loadVisitors(newArry); //רנדור מערך מסונן
-}
-searchAnimalWeight.addEventListener("input", arryFilteringWeight); //האזנה לאירוע שינוי באינפוט
-function arryFilteringWeight(e) {
-  //פונקצייה שמופעלת על ידי האיוונט ליסטינר
-  // cardDiv.innerHTML = ``; //איפוס הדיב של המבקרים
-  let newArry = animalsArry.filter(function (animal) {
-    // יצירת מערך חדש שיכיל את המערך המסונן
-    return animal.weight > e.target.value;
-  });
-  localStorage.setItem("filterd array", JSON.stringify(newArry));
-  // loadVisitors(newArry); //רנדור מערך מסונן
-}
+};
 
-function renderAvailableAnimals() {
-  // ממשו את הלוגיקה שמרנדרת את החיות לתוך הדיב עם האיידי animal-cards
-  // וודאו שאתם מציגים אך ורק את החיות שעומדות בפילטורים הנוכחיים
-  // במידה ואין פילטרים מסומנים, הציגו את כל החיות
-}
-function visitAnimal(animalName) {
-  // ממשו את הלוגיקה של מעבר לעמוד חיה עבור החיה הספציפית שנבחרה
-  // שמרו בלוקל סטורג' את החיה שנבחרה, כך שבעמוד החיה נוכל לשלוף אותה מהסטורג' ולהציגה בהתאם
-}
-
-function setFilter(filterKey, filterValue) {
-  /**
-   * ממשו את הלוגיקה של השמת פילטר
-   * הפילטרים הקיימים הם
-   * isPredator: true | false
-   * habitat: "land" | "sea"
-   * weight: value > user selected weight
-   * height: value > user selected height
-   * color: dropdown of all available colors
-   */
-  // ודאו כי אתם שומרים את הפילטרים שהיוזר בחר בלוקל סטורג׳ וטוענים אותם בהתאם
-  // רנדרו רק את החיות שעומדות בתנאים של הפילטרים
-}
+setupApp();
